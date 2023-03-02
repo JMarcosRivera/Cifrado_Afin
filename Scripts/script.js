36e9ab9e51de24f818af1866e73187b576a1adc0
babyyoda62406 <babyyoda62406@gmail.com>
2023-03-02 18:07:35 -0500

    var for let

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,37 +1,37 @@
 function find_int(x) {
-    var abc = 'abcdefghijklmnñopqrstuvwxyz';
-    var n = abc.length;
+    let abc = 'abcdefghijklmnñopqrstuvwxyz';
+    let n = abc.length;
 
-    for (var i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         if (abc[i] == x) {
             return i;
         }
     }
 }
 function find_char(i) {
-    var abc = 'abcdefghijklmnñopqrstuvwxyz';
+    let abc = 'abcdefghijklmnñopqrstuvwxyz';
     return abc[i];
 }
 
 function cipher(s, a, b) {
-    var ans = "";
-    var n = s.length;
+    let ans = "";
+    let n = s.length;
     s = s.toLowerCase();
     a = parseInt(a);
     b = parseInt(b);
 
-    for (var i = 0; i < n; i++) {
-        var value = find_int(s[i]);
+    for (let i = 0; i < n; i++) {
+        let value = find_int(s[i]);
         value = ((value * a) + b) % 27;
-        var my_char = find_char(value);
+        let my_char = find_char(value);
         ans = ans + my_char.toString();
     }
     return ans;
 }
 function show_answer() {
-    var a = document.getElementById("key_a").value;
-    var b = document.getElementById("key_b").value;
-    var text = document.getElementById("text").value.toString();
+    let a = document.getElementById("key_a").value;
+    let b = document.getElementById("key_b").value;
+    let text = document.getElementById("text").value.toString();
     document.getElementById('cipher').value = cipher(text,a,b);
 }
 
